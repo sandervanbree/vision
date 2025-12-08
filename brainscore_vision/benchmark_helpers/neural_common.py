be1e8ec6f7867b54839a65a6efb5e683e7a8d04d
Sander van Bree <sandervanbree@gmail.com>
2025-12-08 21:57:09 +0100

    Ceiling compatibility check
    Use the ceiling compatibility check in the noise ceiling normalization, asserting whether model metric adequately maps to ceiling metric.

--- brainscore_vision/benchmark_helpers/neural_common.py
+++ brainscore_vision/benchmark_helpers/neural_common.py
@@ -5,7 +5,7 @@ from brainscore_core import Score
 from brainscore_vision.benchmarks import BenchmarkBase
 from brainscore_vision.model_interface import BrainModel
 from .screen import place_on_screen
-
+from .ceiling_check import assert_ceiling_compat
 
 class NeuralBenchmark(BenchmarkBase):
     def __init__(self, identifier, assembly, similarity_metric, visual_degrees, number_of_trials, **kwargs):
@@ -40,12 +40,10 @@ def timebins_from_assembly(assembly):
 
 
 def explained_variance(score: Score, ceiling: Score) -> Score:
-    # ro(X, Y)
-    # = (r(X, Y) / sqrt(r(X, X) * r(Y, Y)))^2
-    # = (r(X, Y) / sqrt(r(Y, Y) * r(Y, Y)))^2  # assuming that r(Y, Y) ~ r(X, X) following Yamins 2014
-    # = (r(X, Y) / r(Y, Y))^2
-    r_square = np.power(score.values /
-                        ceiling.values, 2)
+    # Note: ceiling here is the split-half reliability r(Y, Y), which bounds model explained variance.
+    # Thus, model-to-ceiling normalization first requires bringing score (r) into r^2 before dividing it by the ceiling (see https://osf.io/preprints/psyarxiv/gjk45).
+    assert_ceiling_compat(score, ceiling, warn_only=True)
+    r_square = np.power(score.values, 2) / ceiling.values
     ceiled_score = Score(r_square)
     if 'error' in score.attrs:
         ceiled_score.attrs['error'] = score.attrs['error']
